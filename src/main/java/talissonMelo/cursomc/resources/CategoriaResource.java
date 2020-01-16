259d5d5aec9695dcab58482eacf30e3395403b18
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-16 08:59:19 -0300

    Clone Projeto cursomc and Inserindo uma nova Categoria com POST

--- src/main/java/talissonMelo/cursomc/resources/CategoriaResource.java
+++ src/main/java/talissonMelo/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package talissonMelo.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import talissonMelo.cursomc.domain.Categoria;
 import talissonMelo.cursomc.services.CategoriaService;
@@ -24,5 +28,12 @@ import talissonMelo.cursomc.services.CategoriaService;
 			Categoria obj = service.busar(id);
 			return  ResponseEntity.ok().body(obj);
 
+		}
+		
+		@RequestMapping(method = RequestMethod.POST)
+		public ResponseEntity<Categoria> insert(@RequestBody Categoria obj){
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
 		}
 }
--- src/main/java/talissonMelo/cursomc/services/CategoriaService.java
+++ src/main/java/talissonMelo/cursomc/services/CategoriaService.java
@@ -19,8 +19,12 @@ public class CategoriaService {
 		
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));	
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
 }
