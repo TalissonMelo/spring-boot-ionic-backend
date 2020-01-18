04032e044e933d7d748400794383fcc669b693b0
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-18 14:17:12 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/talissonMelo/cursomc/domain/Pagamento.java
+++ src/main/java/talissonMelo/cursomc/domain/Pagamento.java
@@ -16,27 +16,27 @@ import talissonMelo.cursomc.domain.enums.EstadoPagamaento;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Pagamento implements Serializable{
+public abstract class Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	private Integer id;
 	private Integer estado;
-	
+
 	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
 	private Pedido pedido;
-	
+
 	public Pagamento() {
-		
+
 	}
 
 	public Pagamento(Integer id, EstadoPagamaento estado, Pedido pedido) {
 		super();
 		this.id = id;
-		this.estado = estado.getCod();
+		this.estado = (estado == null) ? null : estado.getCod();
 		this.pedido = pedido;
 	}
 
@@ -88,7 +88,5 @@ public abstract class Pagamento implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
--- src/main/java/talissonMelo/cursomc/services/CategoriaService.java
+++ src/main/java/talissonMelo/cursomc/services/CategoriaService.java
@@ -35,8 +35,14 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
 	}
 
 	public void delete(Integer id) {
