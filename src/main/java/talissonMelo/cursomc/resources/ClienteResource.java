7bde276d8f164cd68e860c929621c4715909d6a0
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-18 15:22:28 -0300

    Inserindo Cliente com POST

--- src/main/java/talissonMelo/cursomc/resources/ClienteResource.java
+++ src/main/java/talissonMelo/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package talissonMelo.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import talissonMelo.cursomc.domain.Cliente;
 import talissonMelo.cursomc.dto.ClienteDTO;
+import talissonMelo.cursomc.dto.ClienteNewDTO;
 import talissonMelo.cursomc.services.ClienteService;
 
 @RestController
@@ -33,6 +36,14 @@ import talissonMelo.cursomc.services.ClienteService;
 			Cliente obj = service.find(id);
 			return  ResponseEntity.ok().body(obj);
 
+		}
+		
+		@RequestMapping(method = RequestMethod.POST)
+		public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+			Cliente obj = service.fromDTO(objDTO);
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
 		}
 
 		@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/talissonMelo/cursomc/services/ClienteService.java
+++ src/main/java/talissonMelo/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import talissonMelo.cursomc.domain.Cidade;
 import talissonMelo.cursomc.domain.Cliente;
+import talissonMelo.cursomc.domain.Endereco;
+import talissonMelo.cursomc.domain.enums.TipoCliente;
 import talissonMelo.cursomc.dto.ClienteDTO;
+import talissonMelo.cursomc.dto.ClienteNewDTO;
 import talissonMelo.cursomc.repositories.ClienteRepository;
+import talissonMelo.cursomc.repositories.EnderecoRepository;
 import talissonMelo.cursomc.services.exceptions.DataIntegrityException;
 import talissonMelo.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 
@@ -28,6 +37,14 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 
+	}
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -62,6 +79,24 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(),TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new  Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
 	}
 
 }
