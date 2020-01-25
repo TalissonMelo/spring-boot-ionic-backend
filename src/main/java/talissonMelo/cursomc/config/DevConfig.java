78f92746231042486cf1435cf2497910df7f71d9
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-01-25 14:58:02 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/talissonMelo/cursomc/config/DevConfig.java
+++ src/main/java/talissonMelo/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import talissonMelo.cursomc.services.DBService;
+import talissonMelo.cursomc.services.EmailService;
+import talissonMelo.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/talissonMelo/cursomc/services/DBService.java
+++ src/main/java/talissonMelo/cursomc/services/DBService.java
@@ -109,8 +109,9 @@ public class DBService {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		//Mudar email para confirmar se email está sendo enviado.
+		Cliente cli1 = new Cliente(null, "Maria Silva", "talisson.cursos@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
