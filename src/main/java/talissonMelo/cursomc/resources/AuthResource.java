562c8079eb3c45317a23f7b5503d06b53097c1e0
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-02-01 14:57:08 -0300

    Refresh token

---
+++ src/main/java/talissonMelo/cursomc/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package talissonMelo.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import talissonMelo.cursomc.security.JWTUtil;
+import talissonMelo.cursomc.security.UserSS;
+import talissonMelo.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
