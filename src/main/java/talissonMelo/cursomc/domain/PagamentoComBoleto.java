6eaeecae27ad8e8e8dc30772aebb0cb00e53367b
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-02-05 09:13:04 -0300

    Corrigindo erro - formado datas

--- src/main/java/talissonMelo/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/talissonMelo/cursomc/domain/PagamentoComBoleto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import talissonMelo.cursomc.domain.enums.EstadoPagamaento;
@@ -13,7 +14,9 @@ import talissonMelo.cursomc.domain.enums.EstadoPagamaento;
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date pagamento;
 	
 	public PagamentoComBoleto() {
--- src/main/java/talissonMelo/cursomc/domain/Pedido.java
+++ src/main/java/talissonMelo/cursomc/domain/Pedido.java
@@ -18,6 +18,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -25,6 +27,8 @@ public class Pedido implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
 
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
